b608841bb275143ba57d0f6ee18069e1b9e8a913
Md. Raisul Islam Rahad <118774591+Raisulll@users.noreply.github.com>
2026-06-24 12:35:31 +0600

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.cpp
@@ -0,0 +1,17 @@
+// bottom up appraoch, hare start with 1 and reach n. 
+// time complexity O(n), space complexity O(1)
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=1) return 1;
+        int prev1=1; 
+        int prev2=1;
+        for(int i=2; i<=n; ++i){
+            int temp=prev1+prev2;
+            prev2=prev1;
+            prev1=temp;
+        }
+        return prev1;
+    }
+};
