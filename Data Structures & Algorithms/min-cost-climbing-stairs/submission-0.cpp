11c99248295ef77a489d251c853bb70e90c9e6d6
Md. Raisul Islam Rahad <118774591+Raisulll@users.noreply.github.com>
2026-06-24 12:52:57 +0600

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+   public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        vector<int> dp(cost.size() + 2,0);
+        return min(costCalc(cost, dp, 0), costCalc(cost, dp, 1));
+    }
+
+    int costCalc(vector<int>& cost, vector<int>& dp, int ind) {
+        if (ind >= cost.size()) return 0;
+        if (dp[ind]) return dp[ind];
+        return dp[ind] = cost[ind] + min(costCalc(cost, dp, ind + 1), costCalc(cost, dp, ind + 2));
+    }
+};
